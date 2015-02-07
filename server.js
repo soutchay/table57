6e4a5ca802a7808d579654d88f5b22caad7fe9af
soutchay <chungvus@gmail.com>
2015-02-07 15:55:45 -0800

    fix get/post

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var app			= express();
 var bodyParser	= require('body-parser');
 var mongoose	= require('mongoose');
 var traitify    = require('traitify');
-// var TrafficPoint= require('./app/models/traffic_point');
 var Renter = require('./app/models/renter');
 
 //Connect to the database
 mongoose.connect('mongodb://brian:mongo@ds039441.mongolab.com:39441/briansdatabase');
+
+//Configure body body-parser
+app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.json());
 
 traitify.setHost("api-sandbox.traitify.com");
 traitify.setVersion("v1");
@@ -21,15 +24,34 @@ traitify.createAssessment(deckId, function(assessment){
     console.log(assessment);
 });
 
-//Configure body body-parser
-app.use(bodyParser.urlencoded({extend:true}));
-app.use(bodyParser.json());
+//Views
+app.set("view engine", 'ejs');
 
+
+//Routers
+var router      = express.Router();
+
+router.route("/renter")
+  .get(function(request, response){
+    Renter.find(function(error, data){
+        if(error) {console.log(error);}
+        response.status(200).json(data);
+    });
+  })
+  .post(function(request, response){
+    console.log(request);
+    var renter = new Renter();
+    renter.name = "brian";
+    renter.save(function(err){
+        if(err){console.log(err);}
+        response.json({message: "post worked"});
+    });
+  });
+
+
+app.use(router);
 //Set port to 8080
 var port = process.env.PORT || 8080;
-
-//Routes for the api
-var router = express.Router();
 
 app.listen(port);
 console.log('Listening to port:', port);
